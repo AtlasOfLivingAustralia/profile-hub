c2e2304b86b202bc70804d962cac72f8aef8cd21
temi <temi.varghese@csiro.au>
2021-07-30 12:59:10 +1000

    AtlasOfLivingAustralia/profile-hub#737  - corrected image metadata URL path for private images

--- grails-app/controllers/au/org/ala/profile/hub/UrlMappings.groovy
+++ grails-app/controllers/au/org/ala/profile/hub/UrlMappings.groovy
@@ -243,7 +243,7 @@ class UrlMappings {
 
         // The following URLs need to match the URLs used by the ala-images-client plugin so that we can view draft
         // and private images (which do not exist in images.ala.org.au) in the plugin as well as ALA images.
-        "/ws/getImageInfo/$imageId" controller: "image", action: [GET: "getImageInfo"]
+        "/ws/image/$imageId" controller: "image", action: [GET: "getImageInfo"]
         "/image/proxyImage/$imageId" controller: "image", action: [GET: "downloadImage"]
         "/profile/$profileId/image/$imageId/tile/$zoom/$x/$y" controller: "image", action: [GET: "getTile"]
         "/opus/$opusId/profile/$profileId/image/$imageId/tile/$zoom/$x/$y" controller: "image", action: [GET: "getTile"]
