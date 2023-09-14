9f28e226a531f2aa901c070e28ad4ca1654c63ae
Steven Choi <steven.choi@csiro.au>
2023-09-14 15:51:43 +1000

    #777 add pojo

---
+++ grails-app/controllers/au/org/ala/profile/domain/CollectionList.groovy
@@ -0,0 +1,12 @@
+package au.org.ala.profile.domain
+
+import groovy.transform.ToString
+
+@ToString
+class CollectionList {
+    String uuid
+    String shortName
+    String title
+    String thumbnailUrl
+    String description
+}
