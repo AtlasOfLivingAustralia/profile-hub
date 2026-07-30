80be9a5ce9020903dc0c98f44e80229a40736d8b
Jack Brinkman <jack.brinkman@csiro.au>
2026-07-30 15:19:48 +1000

    Init React UI

---
+++ src/ui/src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+// biome-ignore lint/style/noNonNullAssertion: Needed for creating root React DOM node
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
